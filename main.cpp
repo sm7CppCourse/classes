abd0c755a8606dd4d20cb82d78b38f218863dcfb
sm7CppCourse <sm7cppcourse@gmail.com>
2015-12-03 12:41:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
 #include "smart_array.h"
+#include "matrix.h"
 #include "complex.h"
 
 int main()
 {
+	// Smart array tests
+	std::cout << "Smart array tests\n";
 	SmartArray<Complex> a;
 	a.pushBack(Complex(1, 1));
 	a.pushBack(Complex(2, 2));
@@ -21,6 +24,25 @@ int main()
 	std::cout << "a = " << a << std::endl;
 	std::cout << "b = " << b << std::endl;
 	std::cout << "c = " << c << std::endl;
+	
+	// Matrix tests	
+	std::cout << "Matrix tests\n";	
+	Matrix <int> m1(2,3); 
+	m1 << 1, 1, 1, 
+	      2, 2, 2;
+
+	std::cout << "m1 = \n" << m1 << "\n";
+
+	Matrix <int> m2(3,4); 
+	m2 << 1, 2, 3, 4, 
+   	      1, 2, 3, 4,
+  	      1, 2, 3, 4;
+
+	std::cout << "m2 = \n" << m2 << "\n";
+
+	Matrix <int> m3 = m1*m2;
+
+	std::cout << "m1*m2 = \n" << m3 << "\n";	
 
 	getchar();
 	return 0;
